ee544e226dabf2cfea2546bbf0fce4e70ad9c3a7
tspeu <tenshotseuropa@gmail.com>
2018-12-27 16:19:33 +0000

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world -)!')
+});
+
+
+
+var server = app.listen(3000, function() {
+    console.log('server running at port :' + server.address().port);
+});
